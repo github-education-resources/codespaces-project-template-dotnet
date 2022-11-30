0ab99bbce157f54548128b6776e3f73b33849b5d
Justin Yoo <justin.yoojh@gmail.com>
2022-11-30 00:23:29 +0000

    Update GHA workflow for GH Pages

---
+++ src/BlazorApp/Models/SocialIcons.cs
@@ -0,0 +1,14 @@
+namespace BlazorApp.Models
+{
+    public class SocialIcons
+    {
+        public virtual string Email { get; set; } = string.Empty;
+        public virtual string DevDotTo { get; set; } = string.Empty;
+        public virtual string GitHub { get; set; } = string.Empty;
+        public virtual string Instagram { get; set; } = string.Empty;
+        public virtual string LinkedIn { get; set; } = string.Empty;
+        public virtual string Medium { get; set; } = string.Empty;
+        public virtual string Twitter { get; set; } = string.Empty;
+        public virtual string YouTube { get; set; } = string.Empty;
+    }
+}
