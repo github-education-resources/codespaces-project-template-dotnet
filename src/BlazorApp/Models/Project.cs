4fee3b85d0f2dbd37b1b3c42da47e97885f89700
David Pine <david.pine@microsoft.com>
2022-12-01 13:18:10 -0600

    Clean up Blazor code a bit

--- src/BlazorApp/Models/Project.cs
+++ src/BlazorApp/Models/Project.cs
@@ -1,14 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+namespace BlazorApp.Models;
 
-namespace BlazorApp.Models
+public class Project
 {
-    public class Project
-    {
-        public string Title { get; set; } = string.Empty;
-        public string Description { get; set; } = string.Empty;
-        public string Url { get; set; } = string.Empty;
-    }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
 }
