24183e640b96dcc775f9b8bdfd16276dba78b22f
David Pine <david.pine@microsoft.com>
2022-12-02 22:34:13 -0600

    Add caching of hero images, encapsulate into service. (#3)

---
+++ src/BlazorApp/Services/HeroImageService.cs
@@ -0,0 +1,27 @@
+using System.Net.Http.Json;
+using BlazorApp.Models;
+
+namespace BlazorApp.Services;
+
+public sealed class HeroImageService : IDisposable
+{
+    private readonly HttpClient _client;
+    private readonly Task<List<HeroImage>?> _getHeroImagesTask;
+
+    public HeroImageService(HttpClient client)
+    {
+        _client = client;
+        _getHeroImagesTask =
+            _client.GetFromJsonAsync<List<HeroImage>>(
+                "sample-data/heroimages.json");
+    }
+
+    internal async Task<HeroImage?> GetHeroAsync(Func<HeroImage, bool> predicate)
+    {
+        var heros = await _getHeroImagesTask;
+        return heros?.FirstOrDefault(predicate);
+    }
+        
+
+    public void Dispose() => _client.Dispose();
+}
