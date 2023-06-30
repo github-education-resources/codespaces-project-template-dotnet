9b464db41084b2cc85238b5758c671de56f4e33a
Justin Yoo <justin.yoojh@gmail.com>
2023-06-30 10:24:56 +0000

    Update on-create.sh

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -27,13 +27,42 @@ sudo chmod +x /usr/local/bin/oh-my-posh
 ## GitHub Copilot CLI ##
 npm install -g @githubnext/github-copilot-cli
 eval "$(github-copilot-cli alias -- "$0")"
+
+### Add GitHub Copilot CLI alias to .zshrc
 echo '
-# Add GitHub Copilot CLI alias
+# Add GitHub Copilot CLI alias to .zshrc
 alias ghcp='github-copilot-cli'
 eval "$(github-copilot-cli alias -- "$0")"
 ' >> $HOME/.zshrc
+
+### Add GitHub Copilot CLI alias to .bashrc
 echo '
-# Add GitHub Copilot CLI alias
+# Add GitHub Copilot CLI alias to .bashrc
 alias ghcp='github-copilot-cli'
 eval "$(github-copilot-cli alias -- "$0")"
 ' >> $HOME/.bashrc
+
+### Add GitHub Copilot CLI alias to PowerShell profile
+mkdir $HOME/.config/powershell
+echo '
+# Add GitHub Copilot CLI alias to PowerShell profile
+Set-Alias ghcp /usr/local/share/nvm/current/bin/github-copilot-cli
+function what-the-shell { ghcp what-the-shell $args }
+function git-assist { ghcp git-assist $args }
+function gh-assist { ghcp gh-assist $args }
+Set-Alias ?? what-the-shell
+Set-Alias git? git-assist
+Set-Alias gh? gh-assist
+' >> $HOME/.config/powershell/Microsoft.PowerShell_profile.ps1
+
+### Add GitHub Copilot CLI alias to Integrated PowerShell profile
+echo '
+# Add GitHub Copilot CLI alias to Integrated PowerShell profile
+Set-Alias ghcp /usr/local/share/nvm/current/bin/github-copilot-cli
+function what-the-shell { ghcp what-the-shell $args }
+function git-assist { ghcp git-assist $args }
+function gh-assist { ghcp gh-assist $args }
+Set-Alias ?? what-the-shell
+Set-Alias git? git-assist
+Set-Alias gh? gh-assist
+' >> $HOME/.config/powershell/Microsoft.VSCode_profile.ps1
