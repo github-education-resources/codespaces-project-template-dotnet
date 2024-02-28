64baa92c07445cd329293f3d53661f9125e422f5
Justin Yoo <justin.yoojh@gmail.com>
2024-02-28 23:47:32 +0900

    Update app to .NET 8.0.2 (#28)
    * Update app to .NET 8.0.2
    
    - Update DevContainer settings
    - Update Blazor WASM app using .NET 8.0.2
    
    * Update DevContainer settings
    
    - Replace tabs to spaces in JSON
    
    * Update typos
    
    - Fix #24 #19

--- .devcontainer/on-create.sh
+++ .devcontainer/on-create.sh
@@ -19,25 +19,7 @@ dotnet restore && dotnet build
 # Uncomment the below to install Azure Static Web Apps CLI. Make sure you have installed node.js
 npm install -g @azure/static-web-apps-cli
 
-## GitHub Copilot CLI ##
-npm install -g @githubnext/github-copilot-cli
-eval "$(github-copilot-cli alias -- "$0")"
-
 ## OH-MY-POSH ##
 # Uncomment the below to install oh-my-posh
 sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 sudo chmod +x /usr/local/bin/oh-my-posh
-
-## Add GitHub Copilot CLI alias to .zshrc
-echo '
-# Add GitHub Copilot CLI alias to .zshrc
-alias ghcp='github-copilot-cli'
-eval "$(github-copilot-cli alias -- "$0")"
-' >> $HOME/.zshrc
-
-## Add GitHub Copilot CLI alias to .bashrc
-echo '
-# Add GitHub Copilot CLI alias to .bashrc
-alias ghcp='github-copilot-cli'
-eval "$(github-copilot-cli alias -- "$0")"
-' >> $HOME/.bashrc
--- src/BlazorApp/Program.cs
+++ src/BlazorApp/Program.cs
@@ -1,7 +1,8 @@
-using Microsoft.AspNetCore.Components.Web;
-using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp;
 using BlazorApp.Services;
+
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
